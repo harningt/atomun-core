b2962b766caee33e1c8117a303730121e288d503
Thomas Harning Jr <harningt@gmail.com>
2017-06-30 02:32:44 -0400

    hash: update to have necessary methods for base58 enhancement

--- src/main/kotlin/us/eharning/atomun/core/utility/Hash.kt
+++ src/main/kotlin/us/eharning/atomun/core/utility/Hash.kt
@@ -18,6 +18,7 @@ package us.eharning.atomun.core.utility
 
 import org.bouncycastle.crypto.digests.RIPEMD160Digest
 import us.eharning.atomun.core.annotations.Beta
+import java.security.DigestException
 import java.security.MessageDigest
 import java.security.NoSuchAlgorithmException
 
@@ -26,6 +27,7 @@ import java.security.NoSuchAlgorithmException
  */
 @Beta
 object Hash {
+    private const val SHA256_DIGEST_LEN = 32
 
     /**
      * Perform the double-hash of the encoded public key per Bitcoin rules.
@@ -59,20 +61,51 @@ object Hash {
      *         offset into the byte array.
      * @param len
      *         number of bytes to process from the byte array.
+     * @param result
+     *         byte array to write results to.
+     * @param resultOffset
+     *         offset into the result byte array.
+     * @param resultLen
+     *         number of bytes set aside for the result byte array.
+     *
+     * @return length of digest added to buffer.
+     */
+    @JvmStatic
+    fun doubleHash(data: ByteArray, offset: Int, len: Int, result: ByteArray, resultOffset: Int, resultLen: Int): Int {
+        try {
+            val digest = MessageDigest.getInstance("SHA-256")
+            digest.update(data, offset, len)
+            /* Perform first digest pass - which resets state */
+            digest.digest(result, resultOffset, resultLen)
+            /* Feed in digest data */
+            digest.update(result, resultOffset, resultLen)
+            /* Perform second digest pass */
+            return digest.digest(result, resultOffset, resultLen)
+        } catch (e: DigestException) {
+            throw Error("Missing SHA-256 / failed setup", e)
+        } catch (e: NoSuchAlgorithmException) {
+            throw Error("Missing SHA-256 / failed setup", e)
+        }
+    }
+
+    /**
+     * Perform a double SHA-256 hash of the given input data.
+     *
+     * @param data
+     *         byte array to process as input.
+     * @param offset
+     *         offset into the byte array.
+     * @param len
+     *         number of bytes to process from the byte array.
      *
      * @return SHA-256 digest of the data used.
      */
     @JvmStatic
     @JvmOverloads
     fun doubleHash(data: ByteArray, offset: Int = 0, len: Int = data.size): ByteArray {
-        try {
-            val digest = MessageDigest.getInstance("SHA-256")
-            digest.update(data, offset, len)
-            return digest.digest(digest.digest())
-        } catch (e: NoSuchAlgorithmException) {
-            throw Error("Missing SHA-256", e)
-        }
-
+        val result = ByteArray(SHA256_DIGEST_LEN)
+        doubleHash(data, offset, len, result, 0, result.size)
+        return result
     }
 
     /**
@@ -97,6 +130,5 @@ object Hash {
         } catch (e: NoSuchAlgorithmException) {
             throw Error("Missing SHA-256", e)
         }
-
     }
 }
